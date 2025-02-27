9daed6b6dfc682620c9ab3cf20834c0a72040545
Sumit <144472439+mulasumit12@users.noreply.github.com>
2025-02-27 13:56:16 +0530

    Update CylinderVolume.java

--- CylinderVolume.java
+++ CylinderVolume.java
@@ -3,17 +3,11 @@ import java.util.Scanner;
 public class CylinderVolume {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the radius of the cylinder: ");
         double radius = scanner.nextDouble();
-
         System.out.print("Enter the height of the cylinder: ");
         double height = scanner.nextDouble();
-
         double volume = Math.PI*Math.pow(radius,2)*height;
-
         System.out.printf("The volume of the cylinder is: "+volume);
-
-        
     }
 }
